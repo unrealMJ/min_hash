8f71b43b3bd5fd3c707f9dd28a94322a292c30aa
MHY <971671352@qq.com>
2022-06-01 12:06:14 +0800

    update figure

--- draw.py
+++ draw.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 def draw(filename, b, r):
-
     f = open(filename)
     lines = f.readlines()
     similarity = []
@@ -17,25 +16,24 @@ def draw(filename, b, r):
         prob[1] = prob[1].strip()
         simi = line[1].split("=")
         simi[1] = simi[1].strip()
-        similarity.append(float(simi[1])/100)
-        probability.append(float(prob[1])/100)
-
+        similarity.append(float(simi[1]) / 100)
+        probability.append(float(prob[1]) / 100)
 
     x = np.linspace(0, 10, 1000)
-    prediction = 1 - (1 - x**r)**b
-
+    prediction = 1 - (1 - x ** r) ** b
 
     plt.xlim((0, 1))
     plt.ylim((0, 1))
     plt.xlabel("Jaccard Similarity")
     plt.ylabel("Probability")
-    plt.xticks(np.linspace(0, 1, 10))
-    plt.yticks(np.linspace(0, 1, 10))
+    plt.xticks(np.linspace(0, 1, 11))
+    plt.yticks(np.linspace(0, 1, 11))
     l1 = plt.plot(x, prediction, color='red', label='theoretical')
     l2 = plt.scatter(similarity, probability, s=10, color='blue', label='experimental')
     plt.legend(loc='upper left')
     plt.savefig('./figure/' + str(b) + '_' + str(r) + '.jpg')
     plt.cla()
+
 
 if __name__ == '__main__':
     path = "./lsh_result"
@@ -44,9 +42,4 @@ if __name__ == '__main__':
         b_r = file.split("_")
         b = int(b_r[0])
         r = int(b_r[1].split(".")[0])
-        draw(path+'/'+file, b, r)
-
-
-
-
-
+        draw(path + '/' + file, b, r)
